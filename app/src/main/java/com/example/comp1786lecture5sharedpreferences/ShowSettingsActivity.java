fb4723cf124a21bc4f60118f366e1a3410a53c2b
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-06-28 09:07:08 +0700

    Display SharedPreferences on ShowSettingsActivity

--- app/src/main/java/com/example/comp1786lecture5sharedpreferences/ShowSettingsActivity.java
+++ app/src/main/java/com/example/comp1786lecture5sharedpreferences/ShowSettingsActivity.java
@@ -5,6 +5,7 @@ import androidx.preference.PreferenceManager;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShowSettingsActivity extends AppCompatActivity {
 
@@ -12,5 +13,17 @@ public class ShowSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_settings);
+
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(this);
+
+        String strPrefs =
+                sharedPreferences.getBoolean("perform_updates", false) +
+                        "\n" + sharedPreferences.getString("updates_interval", "-1") +
+                        "\n" + sharedPreferences.getString("welcome_message", "NULL");
+
+        TextView settingsTextView = (TextView) findViewById(R.id.textSettings);
+
+        settingsTextView.setText(strPrefs);
     }
 }
