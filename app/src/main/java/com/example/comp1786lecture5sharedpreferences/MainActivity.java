73b7eaf9220ae5ec41d2979260f879e294d1a579
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-06-28 08:31:21 +0700

    Add QuickPrefsFragment

--- app/src/main/java/com/example/comp1786lecture5sharedpreferences/MainActivity.java
+++ app/src/main/java/com/example/comp1786lecture5sharedpreferences/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.comp1786lecture5sharedpreferences;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
 
 import android.os.Bundle;
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static class QuickPrefsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey){
+            setPreferencesFromResource(R.xml.preferences,rootKey);
+        }
     }
 }
