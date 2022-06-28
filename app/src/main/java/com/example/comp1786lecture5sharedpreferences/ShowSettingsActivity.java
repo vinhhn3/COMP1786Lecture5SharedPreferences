0b26031acbd9ab424c1f560446448f9e4ef141ef
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-06-28 08:33:01 +0700

    Add ShowSettingsActivity

---
+++ app/src/main/java/com/example/comp1786lecture5sharedpreferences/ShowSettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.comp1786lecture5sharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_settings);
+    }
+}
